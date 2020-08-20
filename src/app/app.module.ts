09fe64ddc286a993a1b6f169887f7e06d8d90a11
igor <44120286+igman123@users.noreply.github.com>
2020-08-20 18:48:06 +0300

    added logo navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import {AlertModule} from 'ngx-bootstrap/alert';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
